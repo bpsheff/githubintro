e1719be267d142ebac277259e91094a845a1ebcf
bpsheff <113429529+bpsheff@users.noreply.github.com>
2022-10-27 17:52:09 +0100

    Classes, I/O Practicals

--- Practical - agentframework.py
+++ Practical - agentframework.py
@@ -5,4 +5,21 @@ Created on Sat Oct 22 11:27:22 2022
 @author: benja
 """
 
-def Agent
+import random
+
+class Agent:
+    def __init__(self):
+        self.x = random.randint(0,99)
+        self.y = random.randint(0,99)
+    def move(self, agents):
+        if random.random() < 0.5:
+            agents.self.y = (agents.self.y + 1) % 100
+        else:
+            agents.self.y = (agents.self.y - 1) % 100
+            
+        if random.random() < 0.5:
+            agents.self.x = (agents.self.x + 1) % 100
+        else:
+            agents.self.x = (agents.self.x - 1) % 100
+            
+a = agentframework.Agent()
